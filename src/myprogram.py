1132ee11df06156e057cc958c9305d9b12fd59a6
Kimberly Cabamalan <cabakim@uw.edu>
2021-02-15 01:41:56 -0800

    cleaned up bugs

--- src/myprogram.py
+++ src/myprogram.py
@@ -5,7 +5,55 @@ import random
 import numpy as np
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 
-class Model:
+class MyModel:
+    """
+    This is a starter model to get you started. Feel free to modify this file.
+    """
+
+    def load_training_data(train_data):
+        data = open(args.train_data, 'r').read()
+        chars = list(set(data))
+        data_size, vocab_size = len(data), len(chars)
+        char_to_ix = { ch:i for i,ch in enumerate(chars) }
+        ix_to_char = { i:ch for i,ch in enumerate(chars) }
+
+        return data, chars, data_size, vocab_size, char_to_ix, ix_to_char
+
+    def load_test_data(fname):
+        data = []
+        with open(fname) as f:
+            for line in f:
+                inp = line[:-1]  # the last character is a newline
+                data.append(inp)
+        return data
+
+    def run_train(data, chars, data_size, vocab_size, char_to_ix, ix_to_char, Wxh, Whh, Why, bh, by):
+        n, p = 0, 0
+        mWxh, mWhh, mWhy = np.zeros_like(Wxh), np.zeros_like(Whh), np.zeros_like(Why)
+        mbh, mby = np.zeros_like(bh), np.zeros_like(by) # memory variables for Adagrad
+        smooth_loss = -np.log(1.0/vocab_size)*seq_length # loss at iteration 0
+        while n<=1000: # 100000
+            # prepare inputs (we're sweeping from left to right in steps seq_length long)
+            if p+seq_length+1 >= len(data) or n == 0:
+                hprev = np.zeros((hidden_size,1)) # reset RNN memory
+                p = 0 # go from start of data
+            inputs = [char_to_ix[ch] for ch in data[p:p+seq_length]]
+            targets = [char_to_ix[ch] for ch in data[p+1:p+seq_length+1]]
+
+            # forward seq_length characters through the net and fetch gradient
+            loss, dWxh, dWhh, dWhy, dbh, dby, hprev = MyModel.lossFun(inputs, targets, vocab_size, Wxh, Whh, Why, bh, by, hprev)
+            smooth_loss = smooth_loss * 0.999 + loss * 0.001
+
+            # perform parameter update with Adagrad
+            for param, dparam, mem in zip([Wxh, Whh, Why, bh, by], [dWxh, dWhh, dWhy, dbh, dby], [mWxh, mWhh, mWhy, mbh, mby]):
+                mem += dparam * dparam
+                param += -learning_rate * dparam / np.sqrt(mem + 1e-8) # adagrad update
+
+            p += seq_length # move data pointer
+            n += 1 # iteration counter
+
+        return hprev
+
     def lossFun(inputs, targets, vocab_size, Wxh, Whh, Why, bh, by, hprev):
         """
         inputs,targets are both list of integers.
@@ -60,7 +108,6 @@ class Model:
             ixes.append(ix)
         return ixes
 
-
     def sample_top3(h, seed_ix, vocab_size, Wxh, Whh, Why, bh, by):
         """
         output the top3 probable next letters
@@ -75,117 +122,135 @@ class Model:
         ixes = np.random.choice(range(vocab_size),3 , p=p.ravel())
         return ixes
 
-class MyModel:
-    """
-    This is a starter model to get you started. Feel free to modify this file.
-    """
+    def save(work_dir, hprev, char_to_ix, ix_to_char, vocab_size, Wxh, Whh, Why, bh, by):
 
-    @classmethod
-    def load_training_data(cls, fname):
-        data = []
-        with open(fname) as f:
-            for line in f:
-                inp = line[:-1]  # the last character is a newline
-                data.append(inp)
-        return data
+        with open(os.path.join(work_dir, 'model.checkpoint.hprev'), 'wt') as f:
+            for val in hprev:
+                f.write(str(val[0]) + "\n")
 
-    @classmethod
-    def load_test_data(cls, fname):
-        # your code here
-        data = []
-        with open(fname) as f:
-            for line in f:
-                inp = line[:-1]  # the last character is a newline
-                data.append(inp)
-        return data
+        with open(os.path.join(work_dir, 'model.checkpoint.vocab'), 'wt') as f:
+            for character in char_to_ix:
+                if character == "\n":
+                    f.write("<NEWLINE>\n")
+                else:
+                    f.write(character + "\n")
+            f.write(str(vocab_size) + "\n")
 
-    @classmethod
-    def write_pred(cls, preds, fname):
+        with open(os.path.join(work_dir, 'model.checkpoint.Wxh'), 'wt') as f:
+            for line in Wxh:
+                for val in line:
+                    f.write(str(val) + "#")
+                f.write("\n")
+
+        with open(os.path.join(work_dir, 'model.checkpoint.Whh'), 'wt') as f:
+            for line in Whh:
+                for val in line:
+                    f.write(str(val) + "#")
+                f.write("\n")
+        with open(os.path.join(work_dir, 'model.checkpoint.Why'), 'wt') as f:
+            for line in Why:
+                for val in line:
+                    f.write(str(val) + "#")
+                f.write("\n")
+        with open(os.path.join(work_dir, 'model.checkpoint.bh'), 'wt') as f:
+            for val in bh:
+                f.write(str(val[0]) + "\n")
+        with open(os.path.join(work_dir, 'model.checkpoint.by'), 'wt') as f:
+            for val in by:
+                f.write(str(val[0]) + "\n")
+
+    def write_pred(preds, fname):
         with open(fname, 'wt') as f:
             for p in preds:
                 f.write('{}\n'.format(p))
 
-    def run_train(self, data, chars, data_size, vocab_size, char_to_ix, ix_to_char):
-        # hyperparameters
-        hidden_size = 100 # size of hidden layer of neurons
-        seq_length = 25 # number of steps to unroll the RNN for
-        learning_rate = 1e-1
 
-        # model parameters
-        Wxh = np.random.randn(hidden_size, vocab_size)*0.01 # input to hidden
-        Whh = np.random.randn(hidden_size, hidden_size)*0.01 # hidden to hidden
-        Why = np.random.randn(vocab_size, hidden_size)*0.01 # hidden to output
-        bh = np.zeros((hidden_size, 1)) # hidden bias
-        by = np.zeros((vocab_size, 1)) # output bias
-
-        n, p = 0, 0
-        mWxh, mWhh, mWhy = np.zeros_like(Wxh), np.zeros_like(Whh), np.zeros_like(Why)
-        mbh, mby = np.zeros_like(bh), np.zeros_like(by) # memory variables for Adagrad
-        smooth_loss = -np.log(1.0/vocab_size)*seq_length # loss at iteration 0
-        while n<=1000: # 100000
-            # prepare inputs (we're sweeping from left to right in steps seq_length long)
-            if p+seq_length+1 >= len(data) or n == 0:
-                hprev = np.zeros((hidden_size,1)) # reset RNN memory
-                p = 0 # go from start of data
-            inputs = [char_to_ix[ch] for ch in data[p:p+seq_length]]
-            targets = [char_to_ix[ch] for ch in data[p+1:p+seq_length+1]]
-
-            print(inputs)
-            print(targets)
-            # forward seq_length characters through the net and fetch gradient
-            loss, dWxh, dWhh, dWhy, dbh, dby, hprev = Model.lossFun(inputs, targets, vocab_size, Wxh, Whh, Why, bh, by, hprev)
-            smooth_loss = smooth_loss * 0.999 + loss * 0.001
-
-            # perform parameter update with Adagrad
-            for param, dparam, mem in zip([Wxh, Whh, Why, bh, by],
-                                                                            [dWxh, dWhh, dWhy, dbh, dby],
-                                                                            [mWxh, mWhh, mWhy, mbh, mby]):
-                mem += dparam * dparam
-                param += -learning_rate * dparam / np.sqrt(mem + 1e-8) # adagrad update
-
-            p += seq_length # move data pointer
-            n += 1 # iteration counter
-
-        return hprev, Wxh, Whh, Why, bh, by
-
-    def run_pred(self, data, hprev, vocab_size, Wxh, Whh, Why, bh, by):
+    def run_pred(data, hprev, char_to_ix, ix_to_char, vocab_size, Wxh, Whh, Why, bh, by):
         # your code here
         preds = []
         for line in data:
             line = line.split()
             char = list(line[len(line)-1])
             i = char[len(char)-1] #get last character of input line
-            sample_ix = Model.sample_top3(hprev, char_to_ix[i], vocab_size, Wxh, Whh, Why, bh, by)
+            sample_ix = MyModel.sample_top3(hprev, char_to_ix[i], vocab_size, Wxh, Whh, Why, bh, by)
             txt = ''.join(ix_to_char[ix] for ix in sample_ix)
 
             preds.append(txt)
         return preds
 
-    def save(self, work_dir, hprev, vocab_size, Wxh, Whh, Why, bh, by):
-        # your code here
-        # this particular model has nothing to save, but for demonstration purposes we will save a blank file
-        with open(os.path.join(work_dir, 'model.checkpoint'), 'wt') as f:
-            f.write(hprev)
-            f.write(vocab_size)
-            f.write(Wxh)
-            f.write(Whh)
-            f.write(Why)
-            f.write(bh)
-            f.write(by)
 
-    @classmethod
-    def load(cls, work_dir):
-        # your code here
-        # this particular model has nothing to load, but for demonstration purposes we will load a blank file
-        with open(os.path.join(work_dir, 'model.checkpoint')) as f:
-            print(f.read())
-        return MyModel()
+    def load(work_dir):
+        with open(os.path.join(work_dir, 'model.checkpoint.hprev')) as f:
+            print("loading hprev")
+            values = []
+            for line in f:
+                values.append([line.split("\n")[0]])
+            hprev = np.array(values).astype(np.float)
+
+        with open(os.path.join(work_dir, 'model.checkpoint.vocab')) as f:
+            print("loading char_to_ix")
+            char_to_ix = {}
+            ix_to_char = {}
+            i = 0
+            f = list(f)
+            for line in f[:-1]:
+                if line == "<NEWLINE>\n":
+                    character = "\n"
+                else:
+                    character = line.split("\n")[0]
+                char_to_ix[character] = i
+                ix_to_char[i] = character
+                i += 1
+
+            vocab_size = int(f[-1].split("\n")[0])
+
+            print(vocab_size)
+
+        with open(os.path.join(work_dir, 'model.checkpoint.Wxh')) as f:
+            print("loading Wxh")
+            values = []
+            for line in f:
+                line = line.split("#")[:-1]
+                values.append(line)
+            Wxh = np.array(values).astype(np.float)
+
+        with open(os.path.join(work_dir, 'model.checkpoint.Whh')) as f:
+            print("loading Whh")
+            values = []
+            for line in f:
+                line = line.split("#")[:-1]
+                values.append(line)
+            Whh = np.array(values).astype(np.float)
+
+        with open(os.path.join(work_dir, 'model.checkpoint.Why')) as f:
+            print("loading Why")
+            values = []
+            for line in f:
+                line = line.split("#")[:-1]
+                values.append(line)
+            Why = np.array(values).astype(np.float)
+
+        with open(os.path.join(work_dir, 'model.checkpoint.bh')) as f:
+            print("loading bh")
+            values = []
+            for line in f:
+                values.append([line.split("\n")[0]])
+            bh = np.array(values).astype(np.float)
+
+        with open(os.path.join(work_dir, 'model.checkpoint.by')) as f:
+            print("loading by")
+            values = []
+            for line in f:
+                values.append([line.split("\n")[0]])
+            by = np.array(values).astype(np.float)
+
+        return hprev, char_to_ix, ix_to_char, vocab_size, Wxh, Whh, Why, bh, by
 
 if __name__ == '__main__':
     parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
     parser.add_argument('mode', choices=('train', 'test'), help='what to run')
     parser.add_argument('--work_dir', help='where to save', default='work')
-    parser.add_argument('--train_data', help='path to train data', default='example/train_input.txt')
+    parser.add_argument('--train_data', help='path to train data', default='example/input.txt')
     parser.add_argument('--test_data', help='path to test data', default='example/input.txt')
     parser.add_argument('--test_output', help='path to write test predictions', default='pred.txt')
     args = parser.parse_args()
@@ -196,79 +261,49 @@ if __name__ == '__main__':
         if not os.path.isdir(args.work_dir):
             print('Making working directory {}'.format(args.work_dir))
             os.makedirs(args.work_dir)
+
         print('Instatiating model')
         model = MyModel()
-        print('Loading training data')
-        train_data = MyModel.load_training_data(args.train_data)
 
-        chars = list(set(train_data))
-        print(chars)
-        data_size, vocab_size = len(train_data), len(chars)
-        # print ('data has %d characters, %d unique.' % (data_size, vocab_size))
-        char_to_ix = { ch:i for i,ch in enumerate(chars) }
-        ix_to_char = { i:ch for i,ch in enumerate(chars) }
+        print('Loading training data')
+        data, chars, data_size, vocab_size, char_to_ix, ix_to_char = MyModel.load_training_data(args.train_data)
+
+        # hyperparameters
+        hidden_size = 128 # size of hidden layer of neurons
+        seq_length = 32 # number of steps to unroll the RNN for
+        learning_rate = 1e-1
+
+        # model parameters
+        Wxh = np.random.randn(hidden_size, vocab_size)*0.01 # input to hidden
+        Whh = np.random.randn(hidden_size, hidden_size)*0.01 # hidden to hidden
+        Why = np.random.randn(vocab_size, hidden_size)*0.01 # hidden to output
+        bh = np.zeros((hidden_size, 1)) # hidden bias
+        by = np.zeros((vocab_size, 1)) # output bias
 
         print('Training')
-        hprev, Wxh, Whh, Why, bh, by = model.run_train(train_data, chars, data_size, vocab_size, char_to_ix, ix_to_char)
-
-
-        chars = list(set(data))
-         data_size, vocab_size = len(data), len(chars)
-         print ('data has %d characters, %d unique.' % (data_size, vocab_size))
-         char_to_ix = { ch:i for i,ch in enumerate(chars) }
-         ix_to_char = { i:ch for i,ch in enumerate(chars) }
-         # hyperparameters
-         hidden_size = 100 # size of hidden layer of neurons
-         seq_length = 25 # number of steps to unroll the RNN for
-         learning_rate = 1e-1
-
-         # model parameters
-         Wxh = np.random.randn(hidden_size, vocab_size)*0.01 # input to hidden
-         Whh = np.random.randn(hidden_size, hidden_size)*0.01 # hidden to hidden
-         Why = np.random.randn(vocab_size, hidden_size)*0.01 # hidden to output
-         bh = np.zeros((hidden_size, 1)) # hidden bias
-         by = np.zeros((vocab_size, 1)) # output bias
-
-         n, p = 0, 0
-         mWxh, mWhh, mWhy = np.zeros_like(Wxh), np.zeros_like(Whh), np.zeros_like(Why)
-         mbh, mby = np.zeros_like(bh), np.zeros_like(by) # memory variables for Adagrad
-         smooth_loss = -np.log(1.0/vocab_size)*seq_length # loss at iteration 0
-         while n<=100000:
-            # prepare inputs (we're sweeping from left to right in steps seq_length long)
-            if p+seq_length+1 >= len(data) or n == 0:
-                hprev = np.zeros((hidden_size,1)) # reset RNN memory
-                p = 0 # go from start of data
-            inputs = [char_to_ix[ch] for ch in data[p:p+seq_length]]
-            targets = [char_to_ix[ch] for ch in data[p+1:p+seq_length+1]]
-
-            # forward seq_length characters through the net and fetch gradient
-            loss, dWxh, dWhh, dWhy, dbh, dby, hprev = Model.lossFun(inputs, targets, hprev)
-            smooth_loss = smooth_loss * 0.999 + loss * 0.001
-            if n % 100 == 0:
-                print('iter %d, loss: %f' % (n, smooth_loss)) # print progress
-
-            # perform parameter update with Adagrad
-            for param, dparam, mem in zip([Wxh, Whh, Why, bh, by],
-                                          [dWxh, dWhh, dWhy, dbh, dby],
-                                          [mWxh, mWhh, mWhy, mbh, mby]):
-                mem += dparam * dparam
-                param += -learning_rate * dparam / np.sqrt(mem + 1e-8) # adagrad update
-
-            p += seq_length # move data pointer
-            n += 1 # iteration counter
+        hprev = MyModel.run_train(data, chars, data_size, vocab_size, char_to_ix, ix_to_char, Wxh, Whh, Why, bh, by)
 
         print('Saving model')
-        model.save(args.work_dir, hprev, vocab_size, Wxh, Whh, Why, bh, by)
+        MyModel.save(args.work_dir, hprev, char_to_ix, ix_to_char, vocab_size, Wxh, Whh, Why, bh, by)
 
     elif args.mode == 'test':
         print('Loading model')
-        hprev, vocab_size, Wxh, Whh, Why, bh, by = MyModel.load(args.work_dir)
+        hprev, char_to_ix, ix_to_char, vocab_size, Wxh, Whh, Why, bh, by = MyModel.load(args.work_dir)
+
         print('Loading test data from {}'.format(args.test_data))
         test_data = MyModel.load_test_data(args.test_data)
+
         print('Making predictions')
-        pred = model.run_pred(test_data, hprev, vocab_size, Wxh, Whh, Why, bh, by)
+        pred = MyModel.run_pred(test_data, hprev, char_to_ix, ix_to_char, vocab_size, Wxh, Whh, Why, bh, by)
+
         print('Writing predictions to {}'.format(args.test_output))
         assert len(pred) == len(test_data), 'Expected {} predictions but got {}'.format(len(test_data), len(pred))
-        model.write_pred(pred, args.test_output)
+
+        print(pred)
+        MyModel.write_pred(pred, args.test_output)
+
+
     else:
         raise NotImplementedError('Unknown mode {}'.format(args.mode))
+
+
--- src/rnn.py
+++ src/rnn.py
@@ -74,3 +74,91 @@ class Model:
         p = np.exp(y) / np.sum(np.exp(y))
         ixes = np.random.choice(range(vocab_size),3 , p=p.ravel())
         return ixes
+
+'''
+class MyModel:
+    """
+    This is a starter model to get you started. Feel free to modify this file.
+    """
+
+    @classmethod
+    def load_training_data(cls, fname):
+        data = []
+        with open(fname) as f:
+            for line in f:
+                inp = line[:-1]  # the last character is a newline
+                data.append(inp)
+        return data
+
+    @classmethod
+    def load_test_data(cls, fname):
+        # your code here
+        data = []
+        with open(fname) as f:
+            for line in f:
+                inp = line[:-1]  # the last character is a newline
+                data.append(inp)
+        return data
+
+    @classmethod
+    def write_pred(cls, preds, fname):
+        with open(fname, 'wt') as f:
+            for p in preds:
+                f.write('{}\n'.format(p))
+
+    def run_train(data, chars, data_size, vocab_size, char_to_ix, ix_to_char):
+        # hyperparameters
+        hidden_size = 100 # size of hidden layer of neurons
+        seq_length = 25 # number of steps to unroll the RNN for
+        learning_rate = 1e-1
+
+        n, p = 0, 0
+        mWxh, mWhh, mWhy = np.zeros_like(Wxh), np.zeros_like(Whh), np.zeros_like(Why)
+        mbh, mby = np.zeros_like(bh), np.zeros_like(by) # memory variables for Adagrad
+        smooth_loss = -np.log(1.0/vocab_size)*seq_length # loss at iteration 0
+        while n<=1000: # 100000
+            # prepare inputs (we're sweeping from left to right in steps seq_length long)
+            if p+seq_length+1 >= len(data) or n == 0:
+                hprev = np.zeros((hidden_size,1)) # reset RNN memory
+                p = 0 # go from start of data
+            inputs = [char_to_ix[ch] for ch in data[p:p+seq_length]]
+            targets = [char_to_ix[ch] for ch in data[p+1:p+seq_length+1]]
+
+            # forward seq_length characters through the net and fetch gradient
+            loss, dWxh, dWhh, dWhy, dbh, dby, hprev = Model.lossFun(inputs, targets, vocab_size, Wxh, Whh, Why, bh, by, hprev)
+            smooth_loss = smooth_loss * 0.999 + loss * 0.001
+
+            # perform parameter update with Adagrad
+            for param, dparam, mem in zip([Wxh, Whh, Why, bh, by],
+                                                                            [dWxh, dWhh, dWhy, dbh, dby],
+                                                                            [mWxh, mWhh, mWhy, mbh, mby]):
+                mem += dparam * dparam
+                param += -learning_rate * dparam / np.sqrt(mem + 1e-8) # adagrad update
+
+            p += seq_length # move data pointer
+            n += 1 # iteration counter
+
+        return hprev, Wxh, Whh, Why, bh, by
+
+    def run_pred(self, data, hprev, vocab_size, Wxh, Whh, Why, bh, by):
+        # your code here
+        preds = []
+        for line in data:
+            line = line.split()
+            char = list(line[len(line)-1])
+            i = char[len(char)-1] #get last character of input line
+            sample_ix = Model.sample_top3(hprev, char_to_ix[i], vocab_size, Wxh, Whh, Why, bh, by)
+            txt = ''.join(ix_to_char[ix] for ix in sample_ix)
+
+            preds.append(txt)
+        return preds
+
+
+    @classmethod
+    def load(cls, work_dir):
+        # your code here
+        # this particular model has nothing to load, but for demonstration purposes we will load a blank file
+        with open(os.path.join(work_dir, 'model.checkpoint')) as f:
+            print(f.read())
+        return MyModel()
+'''
